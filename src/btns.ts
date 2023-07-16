f0b4964a242e9cc40c33d223d28ae03a35b6550c
Dmitrii Isaev <oroboroso@mail.ru>
2023-07-17 00:40:26 +0300

    [] edit contetn

--- src/btns.ts
+++ src/btns.ts
@@ -1,5 +1,20 @@
-let result:number = 0;
+let result: TNum = 0;
+const audio = new Audio("https://www.fesliyanstudios.com/play-mp3/387");
 
-export const calculation = function (bool: boolean | null): number {
-    return bool === null ? result : bool ? result++ : result--;
+export const calculation = function (bool: TBollNull, value: TNum = result): TNum {
+    if (value > 0) {
+        return bool === null ? result : bool ? result += value : result -= value;
+    } else return result = 0;
+}
+
+export const countStep = {
+    1: 1,
+    5: 5,
+    10: 10
+}
+
+export const startTimer = function () {
+    setInterval(()=> {
+        audio.play();
+    }, 1000)
 }
---
+++ src/components/MeCounter.vue
@@ -0,0 +1,72 @@
+<script setup lang="ts">
+import { calculation, countStep, startTimer } from '../btns'
+import { ref } from "vue";
+const count = ref(0);
+
+const updateCount = (bool, value)=> {
+  calculation(bool, value);
+  count.value = calculation(null);
+}
+
+const start = ()=> {
+
+}
+
+
+</script>
+
+<template>
+  <div>
+    <div class="me-counter__timer">
+      <button type="button">{{ count }}</button>
+    </div>
+    <div class="me-btns">
+      <div class="me-btns__block">
+        <template v-for="item in Object.keys(countStep).reverse()">
+          <div @click="updateCount(true, Number(item))">
+            <img src="../assets/btn51.png" />
+          </div>
+        </template>
+      </div>
+      <div class="me-btns__block">
+        <template v-for="item in countStep">
+          <div @click="updateCount(false, item)">
+            <img src="../assets/btn51.png" />
+          </div>
+        </template>
+      </div>
+    </div>
+    <div @click="startTimer">
+      Start
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+
+.me-btns {
+  display: flex;
+  flex-direction: row;
+
+  &__block {
+    display: flex;
+    flex-direction: row;
+    padding: 0 10px;
+
+    div {
+      width: 120px;
+      height: 96px;
+      background-color: #1b3e49;
+      border-radius: 16px;
+      overflow: hidden;
+      cursor: pointer;
+      margin: 0 10px;
+
+      img {
+        width: 100%;
+        height: 100%
+      }
+    }
+  }
+}
+</style>
